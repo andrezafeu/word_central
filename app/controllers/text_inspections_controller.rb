fd76ba8b9cd2e0c7cc33fba7a8b06c5b8deaa1c5
Andreza Feu <andrezafeu@gmail.com>
2016-04-18 19:24:52 -0400

    Add word frequency feature, showing the 10 most common words in a text

--- app/controllers/text_inspections_controller.rb
+++ app/controllers/text_inspections_controller.rb
@@ -7,6 +7,10 @@ class TextInspectionsController < ApplicationController
 		@word_count = @text.split(" ").length
 		@minutes_reading = @word_count/275
 		@conclusion = "The estimated time of reading is #{@minutes_reading} minutes."
+		@frequency = Hash.new(0)
+		@scanned = @text.downcase.scan(/\b[a-z]{1,30}\b/).to_a {|word| @frequency[word] = @frequency[word] + 1}
+		@sorted = @scanned.sort_by {|key, value| value }.reverse
+		@most_frequent_words = @sorted[0..9].join(", ")
 		render "results"
 	end
 end
