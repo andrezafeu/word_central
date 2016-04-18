16fc8b0bbb7d99ab25f7c29fb0915d9e6e31500a
Andreza Feu <andrezafeu@gmail.com>
2016-04-18 16:47:36 -0400

    Update form and submit it to another url

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get '/', to: 'site#home', as: :home
 
   get '/text_inspections/new', to: 'text_inspections#new', as: :new
+
+  post '/text_inspections/add', to: 'text_inspections#add', as: :add
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
