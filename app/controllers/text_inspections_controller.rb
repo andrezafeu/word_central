5997d9c96e88f606fe84577e5e43072ea255912b
Andreza Feu <andrezafeu@gmail.com>
2016-04-18 17:20:43 -0400

    Show wordcount instead of user input

--- app/controllers/text_inspections_controller.rb
+++ app/controllers/text_inspections_controller.rb
@@ -4,6 +4,7 @@ class TextInspectionsController < ApplicationController
 	end
 	def create
 		@text = params[:text_inspection][:user_text]
-		render plain: @text
+		@word_count = @text.split(" ").length
+		render plain: @word_count
 	end
 end
